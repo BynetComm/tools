67151e353575ea6b33635c48fa1f18b1c8349ce1
Oz Pozner <ozpozner@gmail.com>
2015-07-12 17:51:48 +0300

    updating tools Sun 07/12/2015 17:51:48.35

--- op.bat
+++ op.bat
@@ -1,1 +1,13 @@
-ruby c:\tools\op.rb %1 %2 %3 %4 %5 %6 %7 %8 %9 
+@echo off
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    ECHO you are Administrator
+	c:\RailsInstaller\Ruby2.0.0\bin\ruby.exe  c:\bynet\tools\ruby\op.rb %1 %2 %3 %4 %5 %6 %7 %8 %9 
+) ELSE (
+    ECHO you are NOT Administrator. Elevating...
+	PING 127.0.0.1 > NUL 2>&1
+	Nircmd\NIRCMD.exe elevate cmd /k c:\RailsInstaller\Ruby2.0.0\bin\ruby.exe  c:\bynet\tools\ruby\op.rb %1 %2 %3 %4 %5 %6 %7 %8 %9 
+    EXIT /B 1
+)
+
+
