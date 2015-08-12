4675c51725ae0551e477755e2e9029930a809b79
Oz Pozner <ozpozner@gmail.com>
2015-08-12 22:50:07 +0300

    updating tools Wed 08/12/2015 22:50:07.88

--- net/ptc.bat
+++ net/ptc.bat
@@ -10,7 +10,7 @@ if "%fp_h%"=="" (set fp_h = 172.17.50.11) else (set fp_h= %pathtesthost%)
 if "%2" == "" (if "%pathtesthost%"=="" set pathtesthost= %fp_h%  ) else (set pathtesthost= %2) 
 if "%3" == "" (if "%pathtestcontrolport%"=="" set pathtestcontrolport=3236) else (set pathtestcontrolport=%3) 
 if "%7" == "" (if "%pathtestdirecton%"=="" set pathtesdirecton=) else (set pathtestdirecton=)
-if "%6" == "" (if "%pathtestpmode%"=="" set pathtestpmode=--TCP ) else (set pathtestpmode=)
+if "%6" == "" (if "%pathtestpmode%"=="" set pathtestpmode=--tcp ) else (set pathtestpmode=)
 if "%4" == "" (if "%pathtestpackets%"=="" set pathtestpackets=1000) else (set pathtestpackets=%4)
 if "%4" == "" (if "%pathtesttime%"=="" set pathtesttime=10) else (set pathtesttime=%4)
 if "%1" == "" (if "%pathtestbw%"=="" set pathtestbw= 35m) else (set pathtestbw=%1)
@@ -24,7 +24,7 @@ c:\bynet\tools\net\pathtest.exe  -c %pathtesthost%:%pathtestcontrolport% -b %pat
 
 :time
 @echo PTC Ver 0.2 Running [%index%/%maxpathtest%{@%time%}] pathtest  -c %pathtesthost%:%pathtestcontrolport% -b %pathtestbw% --time %pathtestpackets% %pathtestdirecton%   %6 %7 %8 %9 
-c:\bynet\tools\net\pathtest.exe  -c %pathtesthost%:%pathtestcontrolport% -b %pathtestbw% --time %pathtesttime% %pathtestpmode% %pathtestdirecton%  %5 %6 %7 %8 %9 
+c:\bynet\tools\net\pathtest.exe  -c %pathtesthost%:%pathtestcontrolport% -b %pathtestbw% --time %pathtesttime% %5 %6 %7 %8 %9 
 
 REM  > %pathtestfilepathname%%pathtestfileindex%.txt
 set /a index=%index% +1
