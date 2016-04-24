8e0eb8e2450fb9edc290f4c2a7a2100149a1d75b
Oz Pozner <ozpozner@gmail.com>
2016-04-24 22:22:51 +0300

    updating tools Sun 04/24/2016 22:22:51.15

--- CLI/npp.bat
+++ CLI/npp.bat
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-"C:\Program Files (x86)\Notepad++\notepad++.exe" %1 %2 %3 %4
-"C:\tools\Notepad++\notepad++.exe" %1 %2 %3 %4
-=======
 @echo off
 if exist "C:\Program Files (x86)\Notepad++\notepad++.exe" (
     cmd /k "C:\Program Files (x86)\Notepad++\notepad++.exe" %1 %2 %3 %4
@@ -12,4 +8,3 @@ if exist "C:\Program Files (x86)\Notepad++\notepad++.exe" (
 
 
 
->>>>>>> 9666bfcdacc24d7f51f17c8970889e9cd82ad0d1
